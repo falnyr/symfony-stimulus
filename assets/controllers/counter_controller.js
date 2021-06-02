47c7dcddc597fe9d35c14ad291565e428dcaf77a
Jan Richter <falnyr@gmail.com>
2021-06-02 22:07:25 +1200

    feat: actions syntax

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -1,14 +1,11 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
+    count = 0
     static targets = ['count'];
 
-    connect() {
-        this.count = 0;
-
-        this.element.addEventListener('click', () => {
-            this.count++;
-            this.countTarget.innerHTML = this.count;
-        })
+    increment() {
+        this.count++;
+        this.countTarget.innerHTML = this.count;
     }
 }
