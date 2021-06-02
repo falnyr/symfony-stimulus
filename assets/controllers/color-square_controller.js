2947da51a8fa002ddf4a93ce7226c0a63d4c7e35
Jan Richter <falnyr@gmail.com>
2021-06-03 00:03:19 +1200

    feat: state in controller

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -1,7 +1,36 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
+  selectedColorId = null
+
+  static targets = ['colorSquare', 'select']
+
+  connect() {
+    this.selectTarget.classList.add('d-none');
+  }
+
   selectColor(event) {
-    console.log(event);
+    this.setSelectedColor(event.currentTarget.dataset.colorId)
+  }
+
+  setSelectedColor(clickedColorId) {
+
+    if (clickedColorId === this.selectedColorId) {
+      this.findSelectedColorSquare().classList.remove('selected');
+      this.selectedColorId = null;
+      this.selectTarget.value = '';
+      return;
+    }
+
+    this.selectedColorId = clickedColorId
+    this.colorSquareTargets.forEach(element => {
+      element.classList.remove('selected');
+    })
+    this.findSelectedColorSquare().classList.add('selected');
+    this.selectTarget.value = this.selectedColorId
+  }
+
+  findSelectedColorSquare() {
+    return this.colorSquareTargets.find(element => element.dataset.colorId === this.selectedColorId)
   }
 }
